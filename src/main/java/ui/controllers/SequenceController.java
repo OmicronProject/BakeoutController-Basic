c420007be21e79d50862c3f1fb825c12c1b01c9a
Michal Kononenko <michalkononenko@gmail.com>
2017-02-27 12:20:09 -0500

    Wrote docs for the SequenceController.

--- src/main/java/ui/controllers/SequenceController.java
+++ src/main/java/ui/controllers/SequenceController.java
@@ -18,34 +18,62 @@ import ui.Controller;
 import java.text.DecimalFormat;
 
 /**
- * Controls routing and dialogues for the sequence tab
+ * This controller manages operation of the voltage set point algorithm
  */
 @Controller
 public class SequenceController {
+    /**
+     * The application log
+     */
     private static final Logger log = LoggerFactory.getLogger
             (SequenceController.class);
 
+    /**
+     * The kernel to which this controller is attached
+     */
     @Autowired
     private Kernel kernel;
 
+    /**
+     * The algorithm to use
+     */
     @Autowired
     private VoltageSetPointAlgorithm algorithm;
 
+    /**
+     * The text field in which the desired voltage is reported
+     */
     @FXML
     private TextField voltageTextField;
 
+    /**
+     * A slider where the user can interact with voltages
+     */
     @FXML
     private Slider voltageSlider;
 
+    /**
+     * The button to click when the control algorithm is ready to be run
+     */
     @FXML
     private Button goButton;
 
+    /**
+     * A text field where the user can enter the upper bound for the pressure
+     */
     @FXML
     private TextField pressureTextField;
 
+    /**
+     * A slider that complements {@link SequenceController#pressureTextField}
+     */
     @FXML
     private Slider pressureSlider;
 
+    /**
+     * Add listeners to the variable sliders, so that the value in the
+     * {@link TextField} changes with a change in the slider position.
+     */
     @FXML
     public void initialize(){
         voltageSlider.valueProperty().addListener(
@@ -71,14 +99,23 @@ public class SequenceController {
         );
     }
 
+    /**
+     * Set the slider to the position that is in the text field
+     */
     @FXML public void handleVoltageTextFieldChanged() {
-        voltageSlider.setValue(parseTextFieldToDouble());
+        voltageSlider.setValue(parseTextFieldToDouble(voltageTextField));
     }
 
+    /**
+     * Set the position of the pressure slider to that in the text field
+     */
     @FXML public void handlePressureTextFieldChanged(){
-        pressureSlider.setValue(parsePressureTextFieldToDouble());
+        pressureSlider.setValue(parseTextFieldToDouble(pressureTextField));
     }
 
+    /**
+     * Run the algorithm
+     */
     @FXML public void handleGoButtonClicked(){
         algorithm.setKernel(kernel);
         algorithm.setDesiredVoltage(voltageSlider.getValue());
@@ -93,34 +130,48 @@ public class SequenceController {
         }
     }
 
+    /**
+     * If the position of the slider changed, take the value in the slider
+     * and write it to its corresponding text field
+     */
     private void handleVoltageSliderChanged(){
         Double sliderValue = voltageSlider.getValue();
         DecimalFormat format = new DecimalFormat("#.00");
         voltageTextField.setText(format.format(sliderValue));
     }
 
+    /**
+     * If the position of the pressure slider changed, write the changes to
+     * its corresponding text field
+     */
     private void handlePressureSliderChanged(){
         Double sliderValue = pressureSlider.getValue();
         DecimalFormat format = new DecimalFormat("0.00E0");
         pressureTextField.setText(format.format(sliderValue));
     }
 
+    /**
+     * @param field The field from which a number needs to be retrieved
+     * @return The number written in the text field
+     */
     @NotNull
-    private Double parseTextFieldToDouble(){
-        CharSequence seq = voltageTextField.getCharacters();
+    private static Double parseTextFieldToDouble(TextField field){
+        CharSequence seq = field.getCharacters();
         return Double.parseDouble(seq.toString());
     }
 
-    @NotNull
-    private Double parsePressureTextFieldToDouble(){
-        CharSequence sequence = pressureTextField.getCharacters();
-        return Double.parseDouble(sequence.toString());
-    }
-
+    /**
+     * Gray out the go button
+     */
     private void disableGoButton(){
         goButton.setDisable(Boolean.TRUE);
     }
 
+    /**
+     * Log the error out
+     * @param error The error that was thrown while trying to run the
+     *              control algorithm
+     */
     private void handleException(Exception error){
         log.error("Trying to run algorithm threw error", error);
     }
